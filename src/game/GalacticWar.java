23a0bb0068a8b375804f1c829ee2d08466bb4396
tiltedlistener <tiltedlistener@gmail.com>
2014-07-10 00:10:52 -0700

    Finishing up

--- src/game/GalacticWar.java
+++ src/game/GalacticWar.java
@@ -24,6 +24,11 @@ public class GalacticWar extends Game {
 	static int CENTERX = SCREENWIDTH / 2;
 	static int CENTERY = SCREENHEIGHT / 2;
 	
+	// Game States
+	final int GAME_MENU = 0;
+	final int GAME_RUNNING = 1;
+	final int GAME_OVER = 2;
+	
 	// Sprite states
 	final static int STATE_NORMAL = 0;
 	final static int STATE_COLLIDED = 1;
@@ -43,6 +48,12 @@ public class GalacticWar extends Game {
 	final int SPRITE_ASTEROID_MEDIUM = 11;
 	final int SPRITE_ASTEROID_SMALL = 12;
 	final int SPRITE_ASTEROID_TINY = 13;
+	final int SPRITE_POWERUP_SHIELD = 300;
+	final int SPRITE_POWERUP_HEALTH = 301;
+	final int SPRITE_POWERUP_250 = 302;
+	final int SPRITE_POWERUP_500 = 303;
+	final int SPRITE_POWERUP_1000 = 304;
+	final int SPRITE_POWERUP_GUN = 305;
 	
 	// Images
 	ImageEntity background;
@@ -53,6 +64,22 @@ public class GalacticWar extends Game {
 	ImageEntity[] tnyAsteroids;
 	ImageEntity[] explosions;
 	ImageEntity[] shipImage;
+	ImageEntity[] barImage;
+	ImageEntity barFrame;
+	ImageEntity powerupShield;
+	ImageEntity powerupHealth;
+	ImageEntity powerup250;
+	ImageEntity powerup500;
+	ImageEntity powerup1000;
+	ImageEntity powerupGun;
+	
+	// Health and Shield 
+	int health = 20;
+	int shield = 20;
+	int score = 0;
+	int highscore = 0;
+	int firepower = 1;
+	int gameState = GAME_MENU;
 	
 	// Support
 	AffineTransform identity;
@@ -64,13 +91,15 @@ public class GalacticWar extends Game {
 	long collisionTimer = 0;
 	
 	// Keyboard
-	boolean keyDown, keyUp, keyLeft, keyRight, keyFire;	
+	boolean keyDown, keyUp, keyLeft, keyRight, keyFire, keyShield;	
 	
 	public GalacticWar() { 		
 		super(FRAMERATE, SCREENWIDTH, SCREENHEIGHT);
 	}
 	
 	void gameStartup() {
+		
+		// Set variables
 		identity = new AffineTransform();
 		rand = new Random();
 		bigAsteroids = new ImageEntity[5];
@@ -78,15 +107,44 @@ public class GalacticWar extends Game {
 		smlAsteroids = new ImageEntity[3];
 		tnyAsteroids = new ImageEntity[4];
 		explosions = new ImageEntity[2];
-		shipImage = new ImageEntity[2];
+		shipImage = new ImageEntity[3];
+		barImage = new ImageEntity[2];
 		
+		// Health/Shield
+		barFrame = new ImageEntity(this);
+		barFrame.load("images/barframe.png");
+		barImage[0] = new ImageEntity(this);
+		barImage[0].load("images/bar_health.png");
+		barImage[1] = new ImageEntity(this);
+		barImage[1].load("images/bar_shield.png");
+		
+		// Power ups
+		powerupShield = new ImageEntity(this);
+		powerupShield.load("images/powerup_shield2.png");
+		powerupHealth = new ImageEntity(this);
+		powerupHealth.load("images/powerup_cola.png");
+		powerup250 = new ImageEntity(this);
+		powerup250.load("images/powerup_250.png");
+		powerup500 = new ImageEntity(this);
+		powerup500.load("images/powerup_500.png");
+		powerup1000 = new ImageEntity(this);
+		powerup1000.load("images/powerup_1000.png");
+		powerupGun = new ImageEntity(this);
+		powerupGun.load("images/powerup_gun.png");
+		
+		// What it is
 		background = new ImageEntity(this);
-		background.load("images/space.png");
-			
+		background.load("images/bluespace.png");
+		
+		
+		
+		// Ship forms
 		shipImage[0] = new ImageEntity(this);
 		shipImage[0].load("images/spaceship.png");
 		shipImage[1] = new ImageEntity(this);
 		shipImage[1].load("images/ship_thrust.png");
+		shipImage[2] = new ImageEntity(this);
+		shipImage[2].load("images/ship_shield.png");		
 		
 		AnimatedSprite ship = new AnimatedSprite(this, graphics());
 		ship.setSpriteType(SPRITE_SHIP);
@@ -95,17 +153,19 @@ public class GalacticWar extends Game {
 		ship.setFrameHeight(ship.imageHeight());
 		ship.setPosition(new Point2D(CENTERX, CENTERY));
 		ship.setAlive(true);
-		ship.setState(STATE_NORMAL);
+		ship.setState(STATE_EXPLODING);
+		collisionTimer = System.currentTimeMillis();
 		sprites().add(ship);
 		
+		// Bullets
 		bulletImage = new ImageEntity(this);
-		bulletImage.load("images/laser.png");
+		bulletImage.load("images/plasmashot.png");
 		
+		// Explosions
 		explosions[0] = new ImageEntity(this);
 		explosions[0].load("images/explosion.png");
 		explosions[1] = new ImageEntity(this);
 		explosions[1].load("images/explosion2.png");	
-		
 		
 		
 		for(int n=0; n<5;n++) {
@@ -131,20 +191,44 @@ public class GalacticWar extends Game {
 			String tempName = "tiny" + (n+1) + ".png";
 			tnyAsteroids[n].load("images/" + tempName);
 		}
-	
-		for(int n=0;n<ASTEROIDS;n++) {
-			createAsteroid();
-		}
-
+		
+		pauseGame();
 		start();
+		
 	}
 	
+	/**
+	 * GAME STATE
+	 */
 	public void gameShutdown() {
-		
+		// No sound in my version at the moment
 	}
 	
 	public void gameTimeUpdate() {
 		checkInput();
+		if (!gamePaused() && sprites().size() == 1) {
+			resetGame();
+			gameState = GAME_OVER;
+		}
+	}
+	
+	private void resetGame() {
+		AnimatedSprite ship = (AnimatedSprite) sprites().get(0);
+		sprites().clear();
+		ship.setPosition(new Point2D(SCREENWIDTH/2, SCREENHEIGHT/2));
+		ship.setAlive(true);
+		ship.setState(STATE_EXPLODING);
+		collisionTimer = System.currentTimeMillis();
+		ship.setVelocity(new Point2D(0,0));
+		sprites().add(ship);
+		for(int i=0;i<ASTEROIDS;i++) {
+			createAsteroid();
+		}
+		
+		health = 20;
+		shield = 20;
+		score = 0;
+		firepower = 2;
 	}
 	
 	public void gameRefreshScreen() {
@@ -153,7 +237,65 @@ public class GalacticWar extends Game {
 		// AnimatedSprite ship = (AnimatedSprite)sprites().get(0);
 		g2d.drawImage(background.getImage(), 0,0, SCREENWIDTH - 1, SCREENHEIGHT - 1, this);
 		
-		// UNUSED from the book - dispensing with the status printing and therefore the ship object above
+        if (gameState == GAME_MENU) {
+            g2d.setFont(new Font("Verdana", Font.BOLD, 36));
+            g2d.setColor(Color.BLACK);
+            g2d.drawString("GALACTIC WAR", 252, 202);
+            g2d.setColor(new Color(200,30,30));
+            g2d.drawString("GALACTIC WAR", 250, 200);
+
+            int x = 270, y = 15;
+            g2d.setFont(new Font("Times New Roman", Font.ITALIC | Font.BOLD, 20));
+            g2d.setColor(Color.YELLOW);
+            g2d.drawString("CONTROLS:", x, ++y*20);
+            g2d.drawString("ROTATE - Left/Right Arrows", x+20, ++y*20);
+            g2d.drawString("THRUST - Up Arrow", x+20, ++y*20);
+            g2d.drawString("SHIELD - Shift key (no scoring)", x+20, ++y*20);
+            g2d.drawString("FIRE - Ctrl key", x+20, ++y*20);
+
+            g2d.setColor(Color.WHITE);
+            g2d.drawString("POWERUPS INCREASE FIREPOWER!", 240, 480);
+
+            g2d.setFont(new Font("Ariel", Font.BOLD, 24));
+            g2d.setColor(Color.ORANGE);
+            g2d.drawString("Press ENTER to start", 280, 570);
+        }
+        else if (gameState == GAME_RUNNING) {
+            //draw health/shield bars and meters
+            g2d.drawImage(barFrame.getImage(), SCREENWIDTH - 132, 18, this);
+            for (int n = 0; n < health; n++) {
+                int dx = SCREENWIDTH - 130 + n * 5;
+                g2d.drawImage(barImage[0].getImage(), dx, 20, this);
+            }
+            g2d.drawImage(barFrame.getImage(), SCREENWIDTH - 132, 33, this);
+            for (int n = 0; n < shield; n++) {
+                int dx = SCREENWIDTH - 130 + n * 5;
+                g2d.drawImage(barImage[1].getImage(), dx, 35, this);
+            }
+
+            //draw the bullet upgrades
+            for (int n = 0; n < firepower; n++) {
+                int dx = SCREENWIDTH - 220 + n * 13;
+                g2d.drawImage(powerupGun.getImage(), dx, 17, this);
+            }
+
+            //display the score
+            g2d.setFont(new Font("Verdana", Font.BOLD, 24));
+            g2d.setColor(Color.WHITE);
+            g2d.drawString("" + score, 20, 40);
+            g2d.setColor(Color.RED);
+            g2d.drawString("" + highscore, 350, 40);
+        }
+        else if (gameState == GAME_OVER) {
+            g2d.setFont(new Font("Verdana", Font.BOLD, 36));
+            g2d.setColor(new Color(200, 30, 30));
+            g2d.drawString("GAME OVER", 270, 200);
+
+            g2d.setFont(new Font("Arial", Font.CENTER_BASELINE, 24));
+            g2d.setColor(Color.ORANGE);
+            g2d.drawString("Press ENTER to restart", 260, 500);
+        }
+		
 	}
 	
 	/**
@@ -178,6 +320,22 @@ public class GalacticWar extends Game {
 		case SPRITE_ASTEROID_TINY:
 			warp(sprite);
 			break;
+		case SPRITE_POWERUP_SHIELD:	
+		case SPRITE_POWERUP_HEALTH:	
+		case SPRITE_POWERUP_250:	
+		case SPRITE_POWERUP_500:
+		case SPRITE_POWERUP_1000:	
+		case SPRITE_POWERUP_GUN:
+			warp(sprite);
+			double rot = sprite.rotationRate();
+			if(sprite.faceAngle() > 350) {
+				sprite.setRotationRate(rot * -1);
+				sprite.setFaceAngle(350);
+			} else if (sprite.faceAngle() < 10) {
+				sprite.setRotationRate(rot * -1);
+				sprite.setFaceAngle(10);
+			}
+			break;
 		}
 	}
 	
@@ -201,6 +359,7 @@ public class GalacticWar extends Game {
 		switch (spr1.spriteType()) {
 		case SPRITE_BULLET: 
 			if(isAsteroid(spr2.spriteType())) {
+				bumpScore(5);
 				spr1.setAlive(false);
 				spr2.setAlive(false);
 				breakAsteroid(spr2);
@@ -209,12 +368,22 @@ public class GalacticWar extends Game {
 		case SPRITE_SHIP:
 			if(isAsteroid(spr2.spriteType())) {
 				if(spr1.state() == STATE_NORMAL) {
-					collisionTimer = System.currentTimeMillis();
-					spr1.setVelocity(new Point2D(0, 0));
-					double x = spr1.position().X() - 10;
-					double y = spr1.position().Y() - 10;
-					startBigExplosion(new Point2D(x, y));
-					spr1.setState(STATE_EXPLODING);
+					if (keyShield) {
+						shield -=1;
+					} else {
+						collisionTimer = System.currentTimeMillis();
+						spr1.setVelocity(new Point2D(0, 0));
+						double x = spr1.position().X() - 10;
+						double y = spr1.position().Y() - 10;
+						startBigExplosion(new Point2D(x, y));
+						spr1.setState(STATE_EXPLODING);
+						health -=1;
+						if(health < 0){
+							gameState = GAME_OVER;
+						}
+						firepower--;
+						if(firepower < 1) firepower = 1;
+					}
 					spr2.setAlive(false);
 					breakAsteroid(spr2);
 				} else if(spr1.state() == STATE_EXPLODING){
@@ -224,6 +393,50 @@ public class GalacticWar extends Game {
 				}
 			}
 			break;
+        case SPRITE_POWERUP_SHIELD:
+            if (spr2.spriteType()==SPRITE_SHIP) {
+                shield += 5;
+                if (shield > 20) shield = 20;
+                spr1.setAlive(false);
+            }
+            break;
+
+        case SPRITE_POWERUP_HEALTH:
+            if (spr2.spriteType()==SPRITE_SHIP) {
+                health += 5;
+                if (health > 20) health = 20;
+                spr1.setAlive(false);
+            }
+            break;
+
+        case SPRITE_POWERUP_250:
+            if (spr2.spriteType()==SPRITE_SHIP) {
+                bumpScore(250);
+                spr1.setAlive(false);
+            }
+            break;
+
+        case SPRITE_POWERUP_500:
+            if (spr2.spriteType()==SPRITE_SHIP) {
+                bumpScore(500);
+                spr1.setAlive(false);
+            }
+            break;
+
+        case SPRITE_POWERUP_1000:
+            if (spr2.spriteType()==SPRITE_SHIP) {
+                bumpScore(1000);
+                spr1.setAlive(false);
+            }
+            break;
+
+        case SPRITE_POWERUP_GUN:
+            if (spr2.spriteType()==SPRITE_SHIP) {
+                firepower++;
+                if (firepower > 5) firepower = 5;
+                spr1.setAlive(false);
+            }
+            break;			
 		}
 	}
 	
@@ -246,8 +459,34 @@ public class GalacticWar extends Game {
 		case KeyEvent.VK_SPACE:
 			keyFire = true;
 			break;
+		case KeyEvent.VK_SHIFT:
+            if ((!keyUp) && (shield > 0))
+                keyShield = true;
+            else
+                keyShield = false;
+            break;
+
+        case KeyEvent.VK_ENTER:
+            if (gameState == GAME_MENU) {
+                resetGame();
+                resumeGame();
+                gameState = GAME_RUNNING;
+            }
+            else if (gameState == GAME_OVER) {
+                resetGame();
+                resumeGame();
+                gameState = GAME_RUNNING;
+            }
+            break;
+
+        case KeyEvent.VK_ESCAPE:
+            if (gameState == GAME_RUNNING) {
+                pauseGame();
+                gameState = GAME_OVER;
+            }
+            break;			
+			
 		}
-		
 		// I have removed the optional turn on bounds and collision testing
 	}
 	
@@ -266,6 +505,9 @@ public class GalacticWar extends Game {
 		case KeyEvent.VK_SPACE:
 			keyFire = false;
 			fireBullets();
+			break;
+		case KeyEvent.VK_SHIFT:
+			keyShield = false;
 			break;
 		}
 	}
@@ -352,7 +594,63 @@ public class GalacticWar extends Game {
 		
 	
 	private void spawnPowerup(AnimatedSprite sprite) {
+		int n = rand.nextInt(100);
+		if(n > 12) return;
 		
+		AnimatedSprite spr = new AnimatedSprite(this, graphics());
+		spr.setRotationRate(8);
+		spr.setPosition(sprite.position());
+		double velx = rand.nextDouble();
+		double vely = rand.nextDouble();
+		spr.setVelocity(new Point2D(velx, vely));
+		spr.setLifespan(500);
+		spr.setAlive(true);
+		
+		//customize the sprite based on powerup type
+        switch(rand.nextInt(6)) {
+        case 0:
+            //create a new shield powerup sprite
+            spr.setImage(powerupShield.getImage());
+            spr.setSpriteType(SPRITE_POWERUP_SHIELD);
+            sprites().add(spr);
+            break;
+
+        case 1:
+            //create a new health powerup sprite
+            spr.setImage(powerupHealth.getImage());
+            spr.setSpriteType(SPRITE_POWERUP_HEALTH);
+            sprites().add(spr);
+            break;
+
+        case 2:
+            //create a new 250-point powerup sprite
+            spr.setImage(powerup250.getImage());
+            spr.setSpriteType(SPRITE_POWERUP_250);
+            sprites().add(spr);
+            break;
+
+        case 3:
+            //create a new 500-point powerup sprite
+            spr.setImage(powerup500.getImage());
+            spr.setSpriteType(SPRITE_POWERUP_500);
+            sprites().add(spr);
+            break;
+
+        case 4:
+            //create a new 1000-point powerup sprite
+            spr.setImage(powerup1000.getImage());
+            spr.setSpriteType(SPRITE_POWERUP_1000);
+            sprites().add(spr);
+            break;
+
+        case 5:
+            //create a new gun powerup sprite
+            spr.setImage(powerupGun.getImage());
+            spr.setSpriteType(SPRITE_POWERUP_GUN);
+            sprites().add(spr);
+            break;
+
+        }
 	}
 	
 	private void createAsteroid() {
@@ -397,6 +695,8 @@ public class GalacticWar extends Game {
 	 * Process Keys
 	 */
 	public void checkInput() {
+		if (gameState != GAME_RUNNING) return;
+		
 		AnimatedSprite ship = (AnimatedSprite)sprites().get(0);
 		
 		if(keyLeft) {
@@ -415,10 +715,11 @@ public class GalacticWar extends Game {
 		if (keyUp) {
 			ship.setImage(shipImage[1].getImage());
 			applyThrust();
+		} else if(keyShield) {
+			ship.setImage(shipImage[2].getImage());
 		} else {
 			ship.setImage(shipImage[0].getImage());
 		}
-		
 	}
 	
 	public void applyThrust() {
@@ -447,30 +748,132 @@ public class GalacticWar extends Game {
 	}
 	
 	public void fireBullets() {
-		AnimatedSprite ship = (AnimatedSprite)sprites().get(0);
-		
-		AnimatedSprite bullet = new AnimatedSprite(this, graphics());
-		bullet.setImage(bulletImage.getImage());
-		bullet.setFrameWidth(bulletImage.width());
-		bullet.setFrameHeight(bulletImage.height());
-		bullet.setSpriteType(SPRITE_BULLET);
-		bullet.setAlive(true);
-		bullet.setLifespan(200);
-		bullet.setFaceAngle(ship.faceAngle());
-		bullet.setMoveAngle(ship.faceAngle() - 90);
-		
-		double x = ship.center().X() - bullet.imageWidth() / 2;
-		double y = ship.center().Y() - bullet.imageHeight() / 2;
-		bullet.setPosition(new Point2D(x, y));
-		
-		double angle = bullet.moveAngle();
-		double svx = calcAngleMoveX(angle) * BULLET_SPEED;
-		double svy = calcAngleMoveY(angle) * BULLET_SPEED;
-		bullet.setVelocity(new Point2D(svx, svy));
-		
-		sprites().add(bullet);
+        //create the new bullet sprite
+        AnimatedSprite[] bullets = new AnimatedSprite[6];
+
+        switch(firepower) {
+        case 1:
+            bullets[0] = stockBullet();
+            sprites().add(bullets[0]);
+            break;
+
+        case 2:
+            bullets[0] = stockBullet();
+            adjustDirection(bullets[0], -4);
+            sprites().add(bullets[0]);
+
+            bullets[1] = stockBullet();
+            adjustDirection(bullets[1], 4);
+            sprites().add(bullets[1]);
+
+            break;
+
+        case 3:
+            bullets[0] = stockBullet();
+            adjustDirection(bullets[0], -4);
+            sprites().add(bullets[0]);
+
+            bullets[1] = stockBullet();
+            sprites().add(bullets[1]);
+
+            bullets[2] = stockBullet();
+            adjustDirection(bullets[2], 4);
+            sprites().add(bullets[2]);
+
+            break;
+
+        case 4:
+            bullets[0] = stockBullet();
+            adjustDirection(bullets[0], -5);
+            sprites().add(bullets[0]);
+
+            bullets[1] = stockBullet();
+            adjustDirection(bullets[1], 5);
+            sprites().add(bullets[1]);
+
+            bullets[2] = stockBullet();
+            adjustDirection(bullets[2], -10);
+            sprites().add(bullets[2]);
+
+            bullets[3] = stockBullet();
+            adjustDirection(bullets[3], 10);
+            sprites().add(bullets[3]);
+
+            break;
+
+        case 5:
+            bullets[0] = stockBullet();
+            adjustDirection(bullets[0], -6);
+            sprites().add(bullets[0]);
+
+            bullets[1] = stockBullet();
+            adjustDirection(bullets[1], 6);
+            sprites().add(bullets[1]);
+
+            bullets[2] = stockBullet();
+            adjustDirection(bullets[2], -15);
+            sprites().add(bullets[2]);
+
+            bullets[3] = stockBullet();
+            adjustDirection(bullets[3], 15);
+            sprites().add(bullets[3]);
+
+            bullets[4] = stockBullet();
+            adjustDirection(bullets[4], -60);
+            sprites().add(bullets[4]);
+
+            bullets[5] = stockBullet();
+            adjustDirection(bullets[5], 60);
+            sprites().add(bullets[5]);
+            break;
+        }
 	}
 	
+	private void adjustDirection(AnimatedSprite sprite, double angle) {
+		angle = sprite.faceAngle() + angle;
+		if (angle < 0 ) angle += 360;
+		else if (angle > 360) angle -= 360;
+		sprite.setFaceAngle(angle);
+		sprite.setMoveAngle(sprite.faceAngle() - 90);
+		angle = sprite.moveAngle();
+		double svx = calcAngleMoveX(angle) * BULLET_SPEED;
+		double svy = calcAngleMoveY(angle) * BULLET_SPEED;
+		sprite.setVelocity(new Point2D(svx, svy));
+	}
+	
+	private AnimatedSprite stockBullet() {
+		AnimatedSprite ship = (AnimatedSprite) sprites().get(0);
+		AnimatedSprite bul = new AnimatedSprite(this, graphics());
+		bul.setAlive(true);
+		bul.setImage(bulletImage.getImage());
+		bul.setFrameWidth(bulletImage.width());
+		bul.setFrameHeight(bulletImage.height());
+		bul.setSpriteType(SPRITE_BULLET);
+		bul.setLifespan(90);
+		bul.setFaceAngle(ship.faceAngle());
+		bul.setMoveAngle(ship.faceAngle() - 90);
+		
+		double angle = bul.moveAngle();
+		double svx = calcAngleMoveX(angle) * BULLET_SPEED;
+		double svy = calcAngleMoveY(angle) * BULLET_SPEED;
+		bul.setVelocity(new Point2D(svx, svy));
+		
+		double x = ship.center().X() - bul.imageWidth()/2;
+		double y = ship.center().Y() - bul.imageHeight()/2;
+		bul.setPosition(new Point2D(x, y));
+		
+		return bul;
+	}
+	
+	/**
+	 * Scores
+	 */
+	public void bumpScore(int howmuch) {
+		score+= howmuch;
+		if (score > highscore) {
+			highscore = score;
+		}
+	}
 	/**
 	 * Draw methods
 	 */
